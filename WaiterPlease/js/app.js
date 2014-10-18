9974299b8e7207733f251c451a99025eb7239468
vrunjeti <varun_munjeti@yahoo.com>
2014-10-18 08:27:19 -0500

    allergy options

--- WaiterPlease/js/app.js
+++ WaiterPlease/js/app.js
@@ -10,3 +10,14 @@ $('.prefflavors .button').click(function(){
 		$(this).removeClass('active');
 	}
 });
+
+$('.prefallergies .button').click(function(){
+	//alert($(this).css('background-color'));
+	if(!$(this).hasClass('active')){
+		$(this).addClass('active');
+		//alert($(this).css('background-color'));
+	}
+	else {
+		$(this).removeClass('active');
+	}
+});
