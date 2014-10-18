5c374b76954e89cd870ce2535552fcb1dfd49c56
vrunjeti <varun_munjeti@yahoo.com>
2014-10-18 08:03:50 -0500

    options selecting deselecting

--- WaiterPlease/js/app.js
+++ WaiterPlease/js/app.js
@@ -1,2 +1,12 @@
 $(document).foundation();
 
+$('.prefflavors .button').click(function(){
+	//alert($(this).css('background-color'));
+	if(!$(this).hasClass('active')){
+		$(this).addClass('active');
+		//alert($(this).css('background-color'));
+	}
+	else {
+		$(this).removeClass('active');
+	}
+});
