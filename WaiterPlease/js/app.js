fdc3b4b2202c930baa21e6cdfd552c21bbb2a450
vrunjeti <varun_munjeti@yahoo.com>
2014-10-18 12:48:41 -0500

    fading

--- WaiterPlease/js/app.js
+++ WaiterPlease/js/app.js
@@ -1,5 +1,25 @@
 $(document).foundation();
 
+$('#introgo').click(function(){
+	//$('.homepage').addClass('hide');
+	$('.homepage').fadeOut(400, function(){$('.prefflavors').fadeIn()});
+	//window.setTimeout($('.prefflavors').fadeIn(), 400);
+	//$('.prefflavors').removeClass('hide');
+	//$('.prefflavors').fadeIn();
+});
+
+$('#flavorsnext').click(function(){
+	$('.prefflavors').fadeOut(400, function(){$('.prefallergies').fadeIn()});
+	//$('.prefflavors').addClass('hide');
+	//$('.prefallergies').removeClass('hide');
+});
+
+$('#allergiesnext').click(function(){
+	$('.prefallergies').fadeOut(400, function(){$('.location').fadeIn()});
+	//$('.prefallergies').addClass('hide');
+	//$('.location').removeClass('hide');
+});
+
 $('.prefflavors .button').click(function(){
 	//alert($(this).css('background-color'));
 	if(!$(this).hasClass('active')){
